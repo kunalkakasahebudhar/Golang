c29b21a3a0e70d3b174c581de98a7811d49ccac3
kunal udhar <kudhar892@gmail.com>
2025-09-20 21:13:08 +0530

    veriables declaration & prints

--- hello_word.go
+++ hello_word.go
@@ -2,8 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
-
-	fmt.Println("Hello Word")
-
+func hello() {
+	fmt.Println("Hello from hello.go")
 }
---
+++ veriable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func veriables() {
+	var name string = "Alice"
+	age := 25
+	var height float64 = 5.7
+	var isStudent bool
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Is Student:", isStudent)
+}
